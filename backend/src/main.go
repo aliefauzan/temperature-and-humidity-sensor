f251675cfcf93e13777e0c9adf3069a90fc2d013
aliefauzan <afindo.mi01@gmail.com>
2025-02-08 00:48:05 +0800

    enable CORS

--- backend/src/main.go
+++ backend/src/main.go
@@ -10,6 +10,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	err := model.ConnectDB()
 	if err != nil {
@@ -26,6 +39,8 @@ func main() {
 	r.HandleFunc("/update", handler.UpdateData).Methods("POST")
 
 	// Menjalankan server
-	fmt.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	r.Use(enableCORS)
+	serverAddr := ":8080"
+	fmt.Printf("Server started at http://localhost%s\n", serverAddr)
+	log.Fatal(http.ListenAndServe(serverAddr, r))
 }
